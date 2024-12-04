6d91863e3e06a9684cfdf94805e90066635b12bd
Bruno Andrade <brunoandrade.contato@icloud.com>
2024-12-04 00:16:25 -0300

    feat: use link at menu component

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function Menu() {
 	return (
 		<ul className="menu">
-			<li>Home</li>
-			<li>About</li>
-			<li>Contact</li>
+			<li><Link href="/">Home</Link></li>
+			<li><Link href="/about" prefetch>About</Link></li>
+			<li><Link href="/contact">Contact</Link></li>
 		</ul>
 	)
 }
