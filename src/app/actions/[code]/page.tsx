7a3208d34327723c93edf23a1b80bd7b8966945b
Bruno Andrade <brunoandrade.contato@icloud.com>
2025-01-14 22:42:24 -0300

    fix: lint

--- src/app/actions/[code]/page.tsx
+++ src/app/actions/[code]/page.tsx
@@ -1,33 +1,40 @@
-import OrigamidService from "@/services/origamid"
+import OrigamidService from "@/services/origamid";
 
 interface PageProps {
-	params: {
-		code: string
-	}
+  params: Promise<{
+    code: string;
+  }>;
 }
 
 export default async function ProductPage({ params }: PageProps) {
-	const { code } = await params // https://nextjs.org/docs/messages/sync-dynamic-apis
+  const { code } = await params; // https://nextjs.org/docs/messages/sync-dynamic-apis
 
-	const period = 10
-	const data = await new OrigamidService().getActionByCode(code, {
-		next: {
-			revalidate: period, // Seconds
-		}
-	})
-	const { nome, atualizada } = data
+  const period = 10;
+  const data = await new OrigamidService().getActionByCode(code, {
+    next: {
+      revalidate: period, // Seconds
+    },
+  });
+  const { nome, atualizada } = data;
 
-	return (
-		<div>
-			<h2>{nome}</h2>
-			
-			<br/>
-			<p>This .html static file is regenerated each {period} second(s).</p>
-			<p>Once you refresh, server's gonna trigger a new bundle for that .html file, and you'll see the cached version in browser. The most recent is gonna be in .cache folder. </p>
-			<p>So, if you reload the page before the revalidation period once more, you'll se the latest .html file</p>
+  return (
+    <div>
+      <h2>{nome}</h2>
 
-			<br/>
-			<p>Lastest fetch at {atualizada.toLocaleString('pt-BR')}</p>
-		</div>
-	)
+      <br />
+      <p>This .html static file is regenerated each {period} second(s).</p>
+      <p>
+        Once you refresh, server's gonna trigger a new bundle for that .html
+        file, and you'll see the cached version in browser. The most recent is
+        gonna be in .cache folder.{" "}
+      </p>
+      <p>
+        So, if you reload the page before the revalidation period once more,
+        you'll se the latest .html file
+      </p>
+
+      <br />
+      <p>Lastest fetch at {atualizada.toLocaleString("pt-BR")}</p>
+    </div>
+  );
 }
--- src/app/courses/[...attr]/page.tsx
+++ src/app/courses/[...attr]/page.tsx
@@ -1,51 +1,54 @@
-import DynamicRouteCounter from "@/components/dynamic-route-counter"
-import OrigamidService from "@/services/origamid"
-import Link from "next/link"
+import DynamicRouteCounter from "@/components/dynamic-route-counter";
+import OrigamidService from "@/services/origamid";
+import Link from "next/link";
 
 interface CoursePageProps {
-	params: {
-		attr: string[]
-	}
+  params: Promise<{
+    attr: string[];
+  }>;
 }
 
-export default async function CoursePage({params}: CoursePageProps) {
-	const { attr } = await params
+export default async function CoursePage({ params }: CoursePageProps) {
+  const { attr } = await params;
 
-	const attributes = attr.length
+  const attributes = attr.length;
 
-	const courseDescription = String(attr[0])
-	const { aulas, descricao } = await new OrigamidService().getCourseBySlug(courseDescription)
-	
-	if (attributes == 1) {
-		return (
-			<>
-				<h2>"{descricao}" Course</h2>
-				<hr/>
-				<DynamicRouteCounter attr={attr}/>
-				<p>Estas são as aulas de {descricao}</p>
-				<ul>
-					{aulas.map(aula => {
-						return (
-							<li key={Math.random()}>
-								<Link href={`/courses/${courseDescription}/${aula.slug}`}>{aula.descricao}</Link>
-							</li>
-						)
-					})}
-				</ul>
-			</>
-		)
-	}
-	else {
-		const { descricao, tempo } = aulas.find(aula => aula.slug == attr[1])!
+  const courseDescription = String(attr[0]);
+  const { aulas, descricao } = await new OrigamidService().getCourseBySlug(
+    courseDescription
+  );
 
-		return (
-			<>
-				<h2>Lesson "{descricao}"</h2>
-				<hr/>
-				<DynamicRouteCounter attr={attr}/>
-				<h3>Information</h3>
-				<p>Duration: {tempo} min(s)</p>
-			</>
-		)
-	}
+  if (attributes == 1) {
+    return (
+      <>
+        <h2>"{descricao}" Course</h2>
+        <hr />
+        <DynamicRouteCounter attr={attr} />
+        <p>Estas são as aulas de {descricao}</p>
+        <ul>
+          {aulas.map((aula) => {
+            return (
+              <li key={Math.random()}>
+                <Link href={`/courses/${courseDescription}/${aula.slug}`}>
+                  {aula.descricao}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </>
+    );
+  } else {
+    const { descricao, tempo } = aulas.find((aula) => aula.slug == attr[1])!;
+
+    return (
+      <>
+        <h2>Lesson "{descricao}"</h2>
+        <hr />
+        <DynamicRouteCounter attr={attr} />
+        <h3>Information</h3>
+        <p>Duration: {tempo} min(s)</p>
+      </>
+    );
+  }
 }
