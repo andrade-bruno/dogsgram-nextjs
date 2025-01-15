b90f3af67b303bc47755731105f7644ebd579dce
Bruno Andrade <brunoandrade.contato@icloud.com>
2025-01-14 22:42:20 -0300

    feat: example of generateStaticParams usage

--- src/app/actions/page.tsx
+++ src/app/actions/page.tsx
@@ -1,25 +1,34 @@
 import OrigamidService from "@/services/origamid";
 import Link from "next/link";
 
-export default async function ActionPage() {
-	const actions = await new OrigamidService().getActions()
+type StaticParamsResponse = {
+  code: string;
+}[];
 
-	return (
+export async function generateStaticParams(): Promise<StaticParamsResponse> {
+  const actions = await new OrigamidService().getActions();
+
+  return actions.map((action) => ({
+    code: action.simbolo,
+  }));
+}
+
+export default async function ActionPage() {
+  const actions = await new OrigamidService().getActions();
+
+  return (
     <main>
       <h2>Actions</h2>
-			<br/>
-			<ol>
-				{actions.map(action => {
-					return (
-						<Link
-							key={action.id}
-							href={`/actions/${action.simbolo}/`}
-						>
-							<li>{action.nome}</li>
-						</Link>
-					)
-				})}
-			</ol>       
+      <br />
+      <ol>
+        {actions.map((action) => {
+          return (
+            <Link key={action.id} href={`/actions/${action.simbolo}/`}>
+              <li>{action.nome}</li>
+            </Link>
+          );
+        })}
+      </ol>
     </main>
   );
 }
