d5aa142e5d97eadea09ac9709ecab92f26498fd9
Bruno Andrade <brunoandrade.contato@icloud.com>
2025-01-30 21:47:02 -0300

    feat: use priority and dynamic

--- src/app/animals/page.tsx
+++ src/app/animals/page.tsx
@@ -1,6 +1,7 @@
 import OrigamidService from "@/services/origamid";
 import Image from "next/image";
 import Link from "next/link";
+import styles from "./styles.module.css";
 
 export default async function Page() {
   const data = await new OrigamidService().getAnimals();
@@ -9,23 +10,26 @@ export default async function Page() {
     <main>
       <h2>Animals</h2>
       <br />
-      {data.map((i) => {
-        return (
-          <Link key={i.id} href={`/animals/${i.nome}/`}>
-            <div>
-              <p>{i.nome}</p>
-              <Image
-                src={i.imagem}
-                alt={i.descricao}
-                height={200}
-                width={200}
-                sizes="100vw"
-              />
-            </div>
-            <br />
-          </Link>
-        );
-      })}
+      <div className={styles.animals}>
+        {data.map((i, index, _array) => {
+          return (
+            <Link key={i.id} href={`/animals/${i.nome}/`}>
+              <div>
+                <p>{i.nome}</p>
+                <Image
+                  src={i.imagem}
+                  alt={i.descricao}
+                  height={200}
+                  width={200}
+                  sizes="(max-width: 600px) 100vw, 50vw"
+                  priority={index < 2}
+                />
+              </div>
+              <br />
+            </Link>
+          );
+        })}
+      </div>
     </main>
   );
 }
